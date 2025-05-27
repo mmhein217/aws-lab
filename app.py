a51ac46a8efc025521628086790ae2c75b54cd33
Myo Min Hein <mmhein217@gmail.com>
2025-05-28 05:22:21 +0700

    V04 Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! Greeting from V03'
+    return 'Hello, world! Greeting from V04'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
